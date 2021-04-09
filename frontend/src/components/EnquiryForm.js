91d391360a493b46a3b30b4550267f99cc15655c
Babatunde Onasanya <onasanyatunde67@gmail.com>
2021-04-10 00:47:48 +0100

    Styled the image

---
+++ frontend/src/components/EnquiryForm.js
@@ -0,0 +1,66 @@
+import './EnquiryForm.css'
+import { useState } from "react";
+
+const EnquiryForm = () => {
+  const [formDetails, setFormDetails] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formDetails);
+  };
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setFormDetails({ ...formDetails, [name]: value });
+  };
+
+  return (
+    <div className='enquiry-form'>
+      <form onSubmit={handleSubmit}>
+        <div className="form-text-input">
+          <input
+            type="text"
+            placeholder="Enter your Name"
+            name="name"
+            onChange={handleFormChange}
+            value={formDetails.name}
+          />
+        </div>
+        <div className="form-text-input">
+          <input
+            type="email"
+            placeholder="Enter your Email"
+            name="email"
+            value={formDetails.email}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div className="form-text-input">
+          <input
+            type="text"
+            placeholder="Enter Subject"
+            name="subject"
+            onChange={handleFormChange}
+            value={formDetails.subject}
+          />
+        </div>
+        <div className="form-textarea-input">
+          <textarea
+            type="text"
+            placeholder="Enter Message"
+            name="message"
+            onChange={handleFormChange}
+            value={formDetails.message}
+          />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default EnquiryForm;
